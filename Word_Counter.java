6eb61544ed37430a64eae61c5e02d8b6d4b62fef
Bhairisetti Venkatesh <165383351+bhairisetti-venkatesh@users.noreply.github.com>
2024-04-15 19:28:06 +0530

    Add files via upload

---
+++ Word_Counter.java
@@ -0,0 +1,55 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+    public class Word_Counter extends JFrame implements ActionListener {
+
+        private JTextArea textArea;
+        private JButton countButton;
+        private JLabel countLabel;
+
+        public Word_Counter() {
+            super("Word Counter");
+
+            textArea = new JTextArea(20, 40);
+            textArea.setFont(new Font("Arial", Font.PLAIN, 22)); // Source: [2](https://stackoverflow.com/questions/6461506/jtextarea-default-font-very-small-in-windows)
+            countButton = new JButton("Count");
+            countButton.setFont(new Font("Arial Black", Font.PLAIN, 18)); // Source: [2](https://stackoverflow.com/questions/6461506/jtextarea-default-font-very-small-in-windows)
+
+
+            countLabel = new JLabel("Word count: 0");
+            countLabel.setFont(new Font("Arial Black", Font.PLAIN, 18)); // Source: [2](https://stackoverflow.com/questions/6461506/jtextarea-default-font-very-small-in-windows)
+
+
+            JPanel panel = new JPanel();
+            panel.add(new JScrollPane(textArea));
+            panel.add(countButton);
+            panel.add(countLabel);
+            panel.setBackground(Color.lightGray);
+
+            add(panel);
+
+            countButton.addActionListener(this);
+
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            pack();
+            setVisible(true);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+
+            String text = textArea.getText();
+
+            String[] words = text.split("\\s+");
+
+            int count = words.length;
+            countLabel.setText("Word count: " + count);
+        }
+
+        public static void main(String[] args) {
+            new Word_Counter();
+        }
+    }
+
+
